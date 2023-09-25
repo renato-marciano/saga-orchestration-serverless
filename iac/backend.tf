e41f260dca4ac70e322cccc051aa46cabed08eb5
renato-marciano <remarcia@microsoft.com>
2023-09-25 21:23:50 +0000

    updated backend

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "azurerm" {
     # modify the first two values for the storage account
+    resource_group_name  = "rg-ren0925"
     storage_account_name = "stren0925"
-    container_name       = "mystoragecontainer"
+    container_name       = "contren0925"
     key                  = "terraform.tfstate"
   }
 }
