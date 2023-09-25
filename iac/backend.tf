ac16882d5aa69748716478c614632139cc086b44
renato-marciano <remarcia@microsoft.com>
2023-09-25 19:50:01 +0000

    trigger workflow

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    # modify the values for the storage account
+    # modify the first two values for the storage account
     storage_account_name = "stren0925"
     container_name       = "mystoragecontainer"
     key                  = "terraform.tfstate"
